4bc7859a34e094138e570abb575cf1d7fbe0a35e
gup97 <gup97@naver.com>
2022-05-26 16:47:03 +0900

    github pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import SubmitPage from "./pages/SubmitPage";
 import ListPage from "./pages/ListPage";
 import DetailPage from "./pages/DatailPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "components/Header";
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
